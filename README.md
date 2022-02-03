22f58cd86a575d20ec0c8541307aba61dabca863
markeezy <markkhoo95@gmail.com>
2022-02-03 14:55:11 -0800

    Update returned object, sort accounts by followers, refactor

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # who-liked-my-tweet
+
+## Description
+See who liked your tweet. Sorted from most to least followers.
+
+## Technologies
+* Twitter API - [(GET /2/tweets/:id/liking_users)](https://developer.twitter.com/en/docs/twitter-api/tweets/likes/api-reference/get-tweets-id-liking_users)
+* axios
+* dotenv
+
--- index.js
+++ index.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 const axios = require('axios');
 
 const getAllUsers = async (tweet) => {
-
   const data = [];
-  const validData = { data: [] };
-  const error = { error: {} };
-  let value;
-  let callCount = 0;
+  const value = {};
+  const call_count = {
+    count: 0,
+    message: 'Total GET requests made (GET /2/tweets/:id/liking_users)'
+  };
 
   const getUsers = async (tweetID, paginate = '') => {
     if (typeof tweetID === typeof 0) { tweetID = tweetID.toString() }
@@ -15,13 +15,13 @@ const getAllUsers = async (tweet) => {
     const config = {
       method: 'get',
       url: `https://api.twitter.com/2/tweets/${tweetID}/liking_users?user.fields=id,name,username,verified,created_at,public_metrics&max_results=100${paginate.length > 0 ? `&pagination_token=${paginate}` : ''}`,
-      headers: { 
-        'Authorization': `Bearer ${process.env.TWITTER_BEARER_TOKEN}`, 
+      headers: {
+        'Authorization': `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
         'Cookie': `guest_id=${process.env.GUEST_ID}; personalization_id="${process.env.PERSONAL_ID}"`
       }
     };
     const res = await axios(config);
-    callCount++;
+    call_count.count++;
 
     return await res.data
   }
@@ -32,39 +32,53 @@ const getAllUsers = async (tweet) => {
     data.push(...initialData.data);
 
     if (initialData.meta.next_token) {
-      paginate = initialData.meta.next_token
+      paginate = initialData.meta.next_token;
     }
-    
+
     while (paginate) {
       const nextData = await getUsers(tweet, paginate);
-  
+
       if (nextData.data) {
         data.push(...nextData.data);
       }
-      
+
       if (nextData.meta.next_token) {
-        paginate = nextData.meta.next_token
+        paginate = nextData.meta.next_token;
       } else {
-        paginate = null
+        paginate = null;
+      }
+
+      if (call_count.count >= 50) {
+        break;
       }
     }
+
+    data.sort((a, b) => (a.public_metrics.followers_count < b.public_metrics.followers_count ? 1 : -1));
+    value.data = data;
+
+    value.call_count = call_count;
+
+    if (call_count.count >= 50) {
+      call_count.message.concat(`\nNot all accounts found. I limited to 50 max GET requests total.`);
+    }
+
   } catch (err) {
-    error.error.statusCode = err.response.status;
-    error.error.statusText = err.response.statusText;
-    error.error.title = err.response.data.title;
-    error.error.detail = err.response.data.detail;
+    const error = {};
+    error.statusCode = err.response.status;
+    error.statusText = err.response.statusText;
+    error.title = err.response.data.title;
+    error.detail = err.response.data.detail;
+    value.error = error;
+
+    call_count.message.concat(`\nRate Limits at 75 calls every 15 minutes. See Docs.`);
+    value.call_count = call_count;
   }
-
-  validData.data = data;
-  value = error.error.statusCode ? error : validData
-
-  console.log(`Total calls made: ${callCount}`)
 
   return value
 }
 
 // Test Below
-getAllUsers('1486854100264550400')
+getAllUsers('1489127743115563008')
   .then(res => {
     console.log(res)
   })
